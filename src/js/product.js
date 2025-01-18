803821b407b17622cade55394e2d167fb4c6ca1a
Benjamin <gracielvelazquez464@gmail.com>
2025-01-18 10:29:58 -0700

    changes

--- src/js/product.js
+++ src/js/product.js
@@ -1,14 +1,9 @@
-import { setLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-const productId = getParam('product');
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
